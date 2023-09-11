3e3a664312092d1fafa6a2b257e1d68cf97bf696
JoshSJKim <seonjoon.josh.kim@gmail.com>
2023-09-11 01:41:09 -0400

    Add solution to repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function(str, repeat) {
+
+  let output = "";
+
+  if (repeat < 0) {
+    return "ERROR";
+  }
+
+  while (repeat > 0) {
+    output = output += str;
+    repeat--;
+  };
+
+  return output;
 
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
