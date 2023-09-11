74685eb2bf25fce22de7db2f3b20b4f9b3c2d823
JoshSJKim <seonjoon.josh.kim@gmail.com>
2023-09-11 01:56:25 -0400

    Add solution to reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  let newStr = str.split('');
+  let revStr = newStr.reverse();
 
+  return revStr.join('');
 };
 
 // Do not edit below this line
