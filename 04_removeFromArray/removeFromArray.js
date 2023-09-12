34a17cd80857374680038a777616e0fce2df0d59
JoshSJKim <seonjoon.josh.kim@gmail.com>
2023-09-12 01:22:15 -0400

    Add initial solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+let removedArr = [];
 
+const removeFromArray = function(arr, remove) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === remove) {
+      removedArr = arr.splice(arr[i-1], 1)  // 'arr[i-1]' is to get the correct index value to compensate for 0 based indexing
+    }
+  }
+  return arr;
 };
+
+// This code works for the first test, but not the rest.
 
 // Do not edit below this line
 module.exports = removeFromArray;
