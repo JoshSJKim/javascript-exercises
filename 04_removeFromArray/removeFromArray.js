ff40d0a8b6ae0a3e1e49ca94a4951cbee536b009
JoshSJKim <seonjoon.josh.kim@gmail.com>
2023-09-12 02:36:28 -0400

    Add new solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,3 +1,4 @@
+/*
 let removedArr = [];
 
 const removeFromArray = function(arr, remove) {
@@ -9,7 +10,18 @@ const removeFromArray = function(arr, remove) {
   return arr;
 };
 
-// This code works for the first test, but not the rest.
+//This code works for the first test, but not the rest.
+*/
+
+/*
+const removeFromArray = function(arr, remove) {
+  let newArr = arr.filter(item => item !== remove);  // use filter method.
+  return newArr;
+}
+
+// This code works for test 1 and 3, but not the rest
+// I need to find a way to deal with multiple arguments for 'remove', as well as cases where the 'remove' arguments are not in sequential order
+*/
 
 // Do not edit below this line
 module.exports = removeFromArray;
