4d8e7be589328e5cf14a96fcf57aae939bf0da08
JoshSJKim <seonjoon.josh.kim@gmail.com>
2023-09-14 02:07:45 -0400

    Use Math.round() to get the correct decimal place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const convertToCelsius = function(fahrenheit) {
   let celsius = (fahrenheit - 32) * 5/9;
-  return celsius.toFixed(1);
+  return Math.round(celsius * 10) / 10; // Use Math.round() to get decimal place instead for both functions
 };
 
 const convertToFahrenheit = function(celsius) {
   let fahrenheit = (celsius * 9/5) + 32;
-  return fahrenheit.toFixed(1);
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // I was sure this was going to work. But it failed miserably.
