d9438b7457ed596972a6ba910e41646262c225b6
JoshSJKim <seonjoon.josh.kim@gmail.com>
2023-09-14 02:00:24 -0400

    Add initial solution and enable all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * 5/9;
+  return celsius.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (celsius * 9/5) + 32;
+  return fahrenheit.toFixed(1);
 };
+
+// I was sure this was going to work. But it failed miserably.
+// The result returns a string, not a number.
+// Also, it shouldn't provide a decimal if it is a whole number (.0 not required)
+// Apparently, there are some known glitches with `toFixed()`.
 
 // Do not edit below this line
 module.exports = {
