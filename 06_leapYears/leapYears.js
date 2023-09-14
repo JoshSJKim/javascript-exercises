25f242943c457c9d11a69dff6411afd4d84c36e6
JoshSJKim <seonjoon.josh.kim@gmail.com>
2023-09-14 01:10:16 -0400

    Add solution and enable all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  if ( year % 4 === 0 && year % 100 === 0 && year % 400 === 0) {
+    return true;
+  } else if (year % 4 === 0 && year % 100 === 0){
+    return false;
+  } else if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
+
+// It was a bit tricky to get the sequence of the conditions in right order.
+// Then I looked at the actual solution and my logic is broken
+// Code still works, but it's inefficient
 
 // Do not edit below this line
 module.exports = leapYears;
