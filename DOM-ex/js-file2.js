a215a57dabbdf8fd2266d8defb29ada212000d07
JoshSJKim <seonjoon.josh.kim@gmail.com>
2023-09-19 01:23:31 -0400

    Add notes on passing parameters to callback function

--- DOM-ex/js-file2.js
+++ DOM-ex/js-file2.js
@@ -12,18 +12,43 @@ btn.addEventListener('click', () => {
 
 // Using named function for above three methods
 
-
+/*
 function alertFunction() {
   alert("Yay! You did it!");
 };
+*/
 
 
 // Named function method for method 2
 // btn.onclick = alertFunction;
 
 // Named function method for method 3
-btn.addEventListener('click', alertFunction);
+//btn.addEventListener('click', alertFunction); // calls the alertFunction()
+
+
+// Below explains passing a parameter to the function that we are calling and its uses
+
+/*
+The 'e' is an object that references the EVENT itself.
+It displays many useful properties and methods (functions that live inside an object)
 
 btn.addEventListener('click', function (e) {
-  e.target.style.background = 'blue';
+  console.log(e);
+})
+*/
+
+/* 
+'e.target' will display the DOM node that was triggered
+
+btn.addEventListener('click', function (e) {
+  console.log(e.target);
+})
+*/
+
+/*
+You can change the style of a DOM node directly by using the event listener
+
+btn.addEventListener('click', function (e) {
+  e.target.style.background = 'blue'; This will change the background color of the button to blue
 });
+*/
